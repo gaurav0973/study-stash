b49091dac5e92c2b89bb188bd7103f6cd95d8f91
gaurav0973 <codejourneypanda@gmail.com>
2025-07-12 23:17:03 +0530

    stated working on the notes page

--- frontend/src/Body.jsx
+++ frontend/src/Body.jsx
@@ -1,7 +1,6 @@
 import { Outlet } from "react-router-dom"
 import { Navbar } from "./components/common/Navbar"
 import { Footer } from "./components/common/Footer"
-import { useSelector } from "react-redux"
 
 function Body() {
   return (
